8f7c156a33097c6b422cdde7f04ccb4e9cd0b95c
Tristram Norman <tristram@simprints.com>
2025-01-28 15:06:00 +0000

    make templates even bigger

--- app/src/main/java/org/jshobbysoft/cameraalign/MainActivity.kt
+++ app/src/main/java/org/jshobbysoft/cameraalign/MainActivity.kt
@@ -120,7 +120,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Rotate image
-        viewBinding.imageRotateButton.setOnClickListener {
+        viewBinding.cameraFlipButton.setOnClickListener {
             viewBinding.basisImage.rotation += 90f
         }
 
@@ -137,7 +137,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Front/back camera toggle
-        viewBinding.cameraFlipButton.setOnClickListener {
+        viewBinding.imageRotateButton.setOnClickListener {
             cameraSel = if (cameraSel == CameraSelector.DEFAULT_FRONT_CAMERA) {
                 CameraSelector.DEFAULT_BACK_CAMERA
             } else {
@@ -316,8 +316,8 @@ class MainActivity : AppCompatActivity() {
         val basisImageConstraint = when (imageType) {
             "left_ear" -> 0.75f
             "right_ear" -> 0.75f
-            "left_foot" -> 1f
-            "right_foot" -> 1f
+            "left_foot" -> 0.90f
+            "right_foot" -> 0.90f
             "left_hand" -> 0.95f
             "right_hand" -> 0.95f
             "head" -> 0.75f
@@ -335,8 +335,9 @@ class MainActivity : AppCompatActivity() {
         // Hide UI elements that aren't needed in this mode
         viewBinding.imageHflipButton.visibility = View.GONE
         viewBinding.imageVflipButton.visibility = View.GONE
-        viewBinding.imageRotateButton.visibility = View.GONE
+        viewBinding.cameraFlipButton.visibility = View.GONE
         viewBinding.buttonLoadPicture.visibility = View.GONE
+        viewBinding.zoomSeekBar.visibility = View.GONE
     }
 
     /**
